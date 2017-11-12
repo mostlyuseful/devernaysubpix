19a3f30b3718742692cea40bc5223f6dd0de03a6
Maurice-Pascal Sonnemann <mpsonnemann@gmail.com>
2017-11-12 16:57:05 +0100

    Add boost timers to call site. Not needed for usage as library

--- cpp/devernaysubpix/devernaysubpix/main.cpp
+++ cpp/devernaysubpix/devernaysubpix/main.cpp
@@ -6,6 +6,8 @@
 #include <fstream>
 #include <iostream>
 
+#include <boost/timer/timer.hpp>
+
 void write_edge_points(cv::Mat const &background_image,
                        std::vector<EdgeDetector::CurvePoint> const &edges) {
   cv::Mat canvas;
@@ -25,8 +27,9 @@ void write_edge_points(cv::Mat const &background_image,
 void write_chains(cv::Mat const &background_image,
                   EdgeDetector::Chains const &chains) {
 
-  std::vector<cv::Vec3b> const colors = {{255, 0, 0}, {0, 255, 0}, {0, 0, 255},
-                                         {255,255,0},{0,255,255},{255,0,255}};
+  std::vector<cv::Vec3b> const colors = {{255, 0, 0},   {0, 255, 0},
+                                         {0, 0, 255},   {255, 255, 0},
+                                         {0, 255, 255}, {255, 0, 255}};
   size_t color_idx = 0;
 
   cv::Mat canvas;
@@ -51,23 +54,42 @@ int main(int argc, char *argv[]) {
   auto g = cv::imread("zebra_256.tif", 0);
   // auto g = cv::imread("kreis.png", 0);
 
+  boost::timer::auto_cpu_timer* t = new boost::timer::auto_cpu_timer();
   auto grads = E::image_gradient(g, 1.0);
-  auto mag = grads.magnitude();
-  double min, max;
-  cv::minMaxLoc(mag, &min, &max);
-  cv::Mat mag_u8;
-  mag.convertTo(mag_u8, CV_8U, 255 / (max - min), -min);
-  cv::imwrite("magn.tif", mag_u8);
+  std::cout << "image_gradient:" << std::endl;
+  delete t;
 
+  t = new boost::timer::auto_cpu_timer();
   auto mask = grads.threshold(50);
-  cv::imwrite("mask.tif", mask);
+  std::cout << "grads.threshold:" << std::endl;
+  delete t;
 
+  t = new boost::timer::auto_cpu_timer();
   auto edges = E::compute_edge_points(grads, mask);
-  write_edge_points(g, edges);
+  std::cout << "compute_edge_points:" << std::endl;
+  delete t;
 
+  t = new boost::timer::auto_cpu_timer();
   auto links = E::chain_edge_points(edges, grads);
-  auto chains = E::thresholds_with_hysteresis(edges, links, grads, 1, 0.1f);
+  std::cout << "chain_edge_points:" << std::endl;
+  delete t;
 
+  t = new boost::timer::auto_cpu_timer();
+  auto chains = E::thresholds_with_hysteresis(edges, links, grads, 1, 0.1f);
+  std::cout << "thresholds_with_hysteresis:" << std::endl;
+  delete t;
+
+  {
+    auto mag = grads.magnitude();
+    double min, max;
+    cv::minMaxLoc(mag, &min, &max);
+    cv::Mat mag_u8;
+    mag.convertTo(mag_u8, CV_8U, 255 / (max - min), -min);
+    cv::imwrite("magn.tif", mag_u8);
+  }
+
+  cv::imwrite("mask.tif", mask);
+  write_edge_points(g, edges);
   write_chains(g, chains);
 
   return 0;
